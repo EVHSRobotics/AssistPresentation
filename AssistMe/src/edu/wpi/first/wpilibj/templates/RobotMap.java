a2e3ef9cc3804f0bc8bb60050ec76fd7c4ded41b
Kevin Wang <wangkevin448@gmail.com>
2014-08-25 16:05:04 -0700

    initialized project

---
+++ AssistMe/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -0,0 +1,18 @@
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    //xbox controller maps
+    public static final int XBOX_CONTROLLER1 = 1;
+    
+    //drive motors 
+    public static final int DRIVE_MOTOR1 = 1;
+    public static final int DRIVE_MOTOR2 = 2;
+    public static final int DRIVE_MOTOR3 = 3;
+    public static final int DRIVE_MOTOR4 = 4;
+}
